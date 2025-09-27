c5bec1d5f81803bf514d03d0c197da6e9ecad5d9
Ashutosh Dubey <ashutoshdubey133@gmail.com>
2025-09-28 02:27:08 +0530

    Initial Spring Project

---
+++ src/main/java/com/bhumiyajewels/wabot/WabotApplication.java
@@ -0,0 +1,13 @@
+package com.bhumiyajewels.wabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WabotApplication.class, args);
+	}
+
+}
